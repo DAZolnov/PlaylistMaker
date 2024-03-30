cb0551554e8f65ba9b6fcb8eddfc638742f3fcc8
DAZolnov <152647958+DAZolnov@users.noreply.github.com>
2024-03-30 03:36:58 +0300

    Revert "Первый релиз"

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,29 +1,20 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val search = findViewById<Button>(R.id.search)
-        val mediaLibrary = findViewById<Button>(R.id.media_library)
-        val settings = findViewById<Button>(R.id.settings)
-
-        search.setOnClickListener {
-            val searchIntent = Intent(this, SearchActivity::class.java)
-            startActivity(searchIntent)
-        }
-        mediaLibrary.setOnClickListener {
-            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
-            startActivity(mediaLibraryIntent)
-        }
-        settings.setOnClickListener {
-            val settingsIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingsIntent)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
